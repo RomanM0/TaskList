efade727e7a48c4549aadeb9faa02268e6fd519c
RomanM0 <milyanroman834@gmail.com>
2023-07-15 16:50:09 +0300

    Initialize project using Create React App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css'
+function Header(){
+    return (
+        <div>
+        <h1 className='header'>Todo List</h1>
+        </div>
+    )
+}
+export default Header;
